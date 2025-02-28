355349502636a06981703d78199cf976403dbd5f
Dimas <d.karpachev@g.nsu.ru>
2025-02-28 15:33:36 +0700

    All files chaanged
    Added information about rats, snakes and lizards in the file2.txt.
    Needs to add some information about mouses in the file1.txt.
    More information about files in main.cpp.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n See file1.txt and file2.txt for many information about rats and mouses.\n";
 
     return 0;
 }
